5e66b6be745a1aa5e86abafafeb95b49be7dc175
Alexander Mykolaichuk <alexmix3000@gmail.com>
2024-02-08 17:56:26 +0100

    refactor iterator API

--- src/btree.zig
+++ src/btree.zig
@@ -10,7 +10,11 @@ pub fn Btree(comptime T: type, comptime Context: type) type {
 
         handle: *c.struct_btree = undefined,
 
-        pub fn init(max_items: usize, comptime compare: fn (a: *T, b: *T, context: ?*Context) c_int, context: ?*Context) Self {
+        pub fn init(
+            max_items: usize,
+            comptime compare: fn (a: *T, b: *T, context: ?*Context) c_int,
+            context: ?*Context,
+        ) Self {
             const cb = struct {
                 fn comp(a: ?*const anyopaque, b: ?*const anyopaque, ctx: ?*anyopaque) callconv(.C) c_int {
                     return compare(@ptrCast(@alignCast(@constCast(a))), @ptrCast(@alignCast(@constCast(b))), @ptrCast(@alignCast(ctx)));
@@ -64,7 +68,13 @@ pub fn Btree(comptime T: type, comptime Context: type) type {
             return c.btree_count(self.handle);
         }
 
-        pub fn ascend(self: *Self, comptime ContextType: type, context: ?*ContextType, pivot: ?*const anyopaque, comptime iter: fn (a: *T, context: ?*ContextType) bool) bool {
+        pub fn ascend(
+            self: *Self,
+            comptime ContextType: type,
+            context: ?*ContextType,
+            pivot: ?*const anyopaque,
+            comptime iter: fn (a: *T, context: ?*ContextType) bool,
+        ) bool {
             const cb = struct {
                 fn iterator(a: ?*const anyopaque, ctx: ?*anyopaque) callconv(.C) bool {
                     return iter(@ptrCast(@alignCast(@constCast(a))), @ptrCast(@alignCast(ctx)));
@@ -74,7 +84,13 @@ pub fn Btree(comptime T: type, comptime Context: type) type {
             return c.btree_ascend(self.handle, pivot, cb.iterator, context);
         }
 
-        pub fn descent(self: *Self, comptime ContextType: type, context: ?*ContextType, pivot: ?*const anyopaque, comptime iter: fn (a: *T, context: ?*ContextType) bool) bool {
+        pub fn descent(
+            self: *Self,
+            comptime ContextType: type,
+            context: ?*ContextType,
+            pivot: ?*const anyopaque,
+            comptime iter: fn (a: *T, context: ?*ContextType) bool,
+        ) bool {
             const cb = struct {
                 fn iterator(a: ?*const anyopaque, ctx: ?*anyopaque) callconv(.C) bool {
                     return iter(@ptrCast(@alignCast(@constCast(a))), @ptrCast(@alignCast(ctx)));
@@ -133,48 +149,48 @@ pub fn Btree(comptime T: type, comptime Context: type) type {
         pub fn height(self: *Self) usize {
             return c.btree_height(self.handle);
         }
-    };
-}
 
-pub fn Iterator(comptime T: type) type {
-    return struct {
-        const Self = @This();
-
-        handle: *c.struct_btree_iter = undefined,
-
-        pub fn init(comptime Type: type, btree: *Type) Self {
-            return .{
-                .handle = c.btree_iter_new(btree.handle).?,
-            };
+        pub fn iterator(self: *Self) Iterator {
+            return .{ .btree_handle = self.handle };
         }
 
-        pub fn deinit(self: *Self) void {
-            c.btree_iter_free(self.handle);
-        }
+        pub const Iterator = struct {
+            btree_handle: *c.struct_btree = undefined,
+            handle: *c.struct_btree_iter = undefined,
 
-        pub fn first(self: *Self) bool {
-            return c.btree_iter_first(self.handle);
-        }
+            pub fn init(self: *const Iterator) Iterator {
+                return .{
+                    .handle = c.btree_iter_new(self.btree_handle).?,
+                };
+            }
+            pub fn deinit(self: *const Iterator) void {
+                c.btree_iter_free(self.handle);
+            }
 
-        pub fn last(self: *Self) bool {
-            return c.btree_iter_last(self.handle);
-        }
+            pub fn first(self: *const Iterator) bool {
+                return c.btree_iter_first(self.handle);
+            }
 
-        pub fn next(self: *Self) bool {
-            return c.btree_iter_next(self.handle);
-        }
+            pub fn last(self: *const Iterator) bool {
+                return c.btree_iter_last(self.handle);
+            }
 
-        pub fn prev(self: *Self) bool {
-            return c.btree_iter_prev(self.handle);
-        }
+            pub fn next(self: *const Iterator) bool {
+                return c.btree_iter_next(self.handle);
+            }
 
-        pub fn seek(self: *Self, key: *const T) bool {
-            return c.btree_iter_seek(self.handle, key);
-        }
+            pub fn prev(self: *const Iterator) bool {
+                return c.btree_iter_prev(self.handle);
+            }
 
-        pub fn item(self: *Self) ?*T {
-            return @ptrCast(@alignCast(@constCast(c.btree_iter_item(self.handle))));
-        }
+            pub fn seek(self: *const Iterator, key: *const T) bool {
+                return c.btree_iter_seek(self.handle, key);
+            }
+
+            pub fn item(self: *const Iterator) ?*T {
+                return @ptrCast(@alignCast(@constCast(c.btree_iter_item(self.handle))));
+            }
+        };
     };
 }
 
@@ -349,7 +365,7 @@ test "iterator" {
 
     _ = btree.ascend(std.ArrayList(u8), &ctx, null, cb.iter);
 
-    var iter = Iterator(User).init(Btree(User, void), &btree);
+    const iter = btree.iterator().init();
 
     try std.testing.expectEqual(iter.seek(&user1), true);
     try std.testing.expectEqual(iter.item().?.id, 1);
